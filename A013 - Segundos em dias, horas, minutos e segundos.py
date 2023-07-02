77c8d14998e1d6866d6e0c0389ac673a4dc454ba
Pedro Candido <134300577+PeCandido@users.noreply.github.com>
2023-07-02 15:10:34 -0300

    Delete A013 - Segundos em dias, horas, minutos e segundos.py

--- A013 - Segundos em dias, horas, minutos e segundos.py
+++
@@ -1,7 +0,0 @@
-segundos = int(input())
-dias = segundos // 86400
-horas = (segundos % 86400) // 3600
-minutos = (segundos % 86400) % 3600 // 60
-seg = segundos % 86400 % 3600 % 60
-
-print(dias, horas, minutos, seg)
