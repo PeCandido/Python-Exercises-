e091a6c8c367de849217beaab705a5ee9a4b7a99
Pedro Candido <134300577+PeCandido@users.noreply.github.com>
2023-07-02 15:10:03 -0300

    Delete A009 - Equação do 2º grau.py

--- A009 - Equação do 2º grau.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-b = int(input())
-c = int(input())
-
-bhask1 = (-b + ((b ** 2) - (4 * a * c)) ** 0.5) / (2 * a)
-bhask2 = (-b - ((b ** 2) - (4 * a * c)) ** 0.5) / (2 * a)
-
-print(f"{bhask1:.2f}")
-print(f"{bhask2:.2f}")
