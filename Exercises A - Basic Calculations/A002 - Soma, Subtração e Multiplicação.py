881d8f2a931290c66141a26e90acc6c375749e49
Pedro Candido <134300577+PeCandido@users.noreply.github.com>
2023-07-02 16:03:32 -0300

    Update A002 - Soma, Subtração e Multiplicação.py

--- Exercises A - Basic Calculations/A002 - Soma, Subtração e Multiplicação.py
+++ Exercises A - Basic Calculations/A002 - Soma, Subtração e Multiplicação.py
@@ -1,10 +1,15 @@
+# soma, subtração e multiplicação de dois valores inteiros
+
+# ENTRADA
 n1 = int(input())
 n2 = int(input())
 
+# PROCESSAMENTO
 soma = n1 + n2
 sub = n1 - n2
 prod = n1 * n2
 
+# SAÍDA
 print(soma)
 print(sub)
 print(prod)
