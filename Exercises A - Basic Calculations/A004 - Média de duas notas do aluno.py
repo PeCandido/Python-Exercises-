32a4d12df420520aa8a9627a1ff4bf6389ea7788
Pedro Candido <134300577+PeCandido@users.noreply.github.com>
2023-07-02 16:05:48 -0300

    Update A004 - Média de duas notas do aluno.py

--- Exercises A - Basic Calculations/A004 - Média de duas notas do aluno.py
+++ Exercises A - Basic Calculations/A004 - Média de duas notas do aluno.py
@@ -1,7 +1,12 @@
+# média de duas notas de um aluno
+
+# ENTRADA
 nome = input()
 n1 = float(input())
 n2 = float(input())
 
+# PROCESSAMENTO
 res = (n1 + n2)/2
 
+# SAÍDA
 print(f"{nome} obteve {res:.2f} de media")
